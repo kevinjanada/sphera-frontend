d4cbaacdac61edd6572fe4bf9f91ab1d722c0a30
Kevin Janada <kevin.janada@gmail.com>
2019-06-16 07:05:10 +0700

    Edit Jadwal

--- src/router/routes.js
+++ src/router/routes.js
@@ -153,7 +153,7 @@ const routes = [
     // beforeEnter: checkAuth,
     meta: {
       breadcrumbs: [
-        { title: 'Insert Lapangan' }
+        { title: 'Edit Venue' }
       ]
     }
   },
--- src/views/InsertLapangan.vue
+++ src/views/InsertLapangan.vue
@@ -1,8 +1,8 @@
 <template>
   <v-layout justify-center fill-height align-center>
     <v-card class="px-4 py-4 elevation-5" width="40vw">
-       <v-layout class="pt-1 display-1 font-weight-black" justify-center id="card-header">
-           ADD NEW FIELD
+      <v-layout class="pt-1 display-1 font-weight-black" justify-center id="card-header">
+        ADD NEW FIELD
       </v-layout>
       <v-alert
         type="error"
@@ -13,37 +13,16 @@
       <v-card-text>
         <v-form @keyup.enter.native="validateBeforeSubmit">
         <!--Owner Name tolong diambil dari session dong :)-->
-          <v-text-field
-            label="Owner Name"
-            prepend-icon="perm_identity"
-            color="#0280e0"
-            autocomplete=""
-            data-vv-name="OwnerName"
-            v-validate="'required'"
-            :error-messages="errors.collect('OwnerName')"
-            type="text"
-            v-model="model.OwnerName"
-          ></v-text-field>  
           <v-select
-          :items="items"
-          label="Venue Name"
-          prepend-icon="home"
-          color="#0280e0"
-          v-validate="'required'"
-          :error-messages="errors.collect('VenueName')"
-          v-model="model.VenueName"
-          ></v-select>
-          <v-text-field
-            label="Field Name"
+            :items="fieldTypes"
+            label="Field Type"
             prepend-icon="home"
             color="#0280e0"
-            autocomplete="FieldName"
-            type="text"
-            data-vv-name="FieldName"
-            v-validate="'required|min:30'"
-            :error-messages="errors.collect('FieldName')"
-            v-model="model.FieldName"
-          ></v-text-field>
+            v-validate="'required'"
+            data-vv-name="FieldType"
+            :error-messages="errors.collect('FieldType')"
+            v-model="model.fieldType"
+          ></v-select>
           <v-text-field
             label="Field Fee per Hour"
             prepend-icon="attach_money"
@@ -51,9 +30,9 @@
             autocomplete="FieldFee"
             type="number"
             data-vv-name="FieldFee"
-            v-validate="'required|min:6'"
-            :error-messages="errors.collect('Password')"
-            v-model="model.FieldFiee"
+            v-validate="'required'"
+            :error-messages="errors.collect('FieldFee')"
+            v-model="model.price"
           ></v-text-field>
         </v-form>
         <v-layout justify-center row wrap class="py-3">
@@ -76,22 +55,34 @@ import tokenService from '../common/tokenService'
 export default {
   data: () => ({
     //ini nanti didapat dari db sesuai OwnerID
-    items: ['Venue A','Venue B','Venue C'],
+    fieldTypes: ['cement', 'grass'],
     loading: false,
     alert: {
       message: null,
       show: false,
     },
     model: {
-      OwnerName: '',
-      FieldName: '',
-      FieldFee:'',
-      password:''
+      venueId: null,
+      fieldType: null,
+      price: null,
     },
     tokenRequest: false,
   }),
+  mounted () {
+    this.model.venueId = this.$route.params.venueId;
+  },
   methods: {
-
+    async validateBeforeSubmit () {
+      const res = await this.$axios.post('/inputLapangan', {
+        venueId: this.model.venueId,
+        fieldType: this.model.fieldType,
+        price: this.model.price,
+      })
+      if (res.data.success) {
+        console.log(res.data)
+        this.$router.push({ name: 'edit-venue', params: { venueId: this.model.venueId } })
+      } 
+    }
   }
 }
 </script>
